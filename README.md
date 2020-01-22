f68b5a3f91a13f67499d9cd22fa4068b0fb130ad
ftreml <florian.treml@botium.at>
2020-01-22 17:26:57 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# botium-connector-fbmessengerbots
+# Botium Connector for Facebook Messenger Bots 
+
+[![NPM](https://nodei.co/npm/botium-connector-fbmessengerbots.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/fbmessengerbots-connector-fbmessengerbots/)
+
+[![npm version](https://badge.fury.io/js/botium-connector-fbmessengerbots.svg)](https://badge.fury.io/js/botium-connector-fbmessengerbots)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)]()
+
+This is a [Botium](https://github.com/codeforequity-at/botium-core) connector for testing your Facebook Messenger Bots.
+
+__Did you read the [Botium in a Nutshell](https://medium.com/@floriantreml/botium-in-a-nutshell-part-1-overview-f8d0ceaf8fb4) articles ? Be warned, without prior knowledge of Botium you won't be able to properly use this library!__
+
+## How it works ?
+Botium uses the [_Unofficial Facebook Chat API_](https://github.com/Schmavery/facebook-chat-api) to connect to your chatbot on Facebook Messenger.
+
+It can be used as any other Botium connector with all Botium Stack components:
+* [Botium CLI](https://github.com/codeforequity-at/botium-cli/)
+* [Botium Bindings](https://github.com/codeforequity-at/botium-bindings/)
+* [Botium Box](https://www.botium.at)
+
+## Requirements
+
+* __Node.js and NPM__
+* a __Facebook Messenger Bot__
+* a __project directory__ on your workstation to hold test cases and Botium configuration
+
+## Install Botium and Facebook Messenger Bots Connector
+
+When using __Botium CLI__:
+
+```
+> npm install -g botium-cli
+> npm install -g botium-connector-fbmessengerbots
+> botium-cli init
+> botium-cli run
+```
+
+When using __Botium Bindings__:
+
+```
+> npm install -g botium-bindings
+> npm install -g botium-connector-fbmessengerbots
+> botium-bindings init mocha
+> npm install && npm run mocha
+```
+
+When using __Botium Box__:
+
+_Already integrated into Botium Box, no setup required_
+
+## Connecting your Facebook bot to Botium
+
+```
+{
+  "botium": {
+    "Capabilities": {
+      "PROJECTNAME": "<whatever>",
+      "CONTAINERMODE": "fbmessengerbots",
+      "FB_PAGEID": "...",
+      "FB_USER": "...",
+      "FB_PASSWORD": "..."      
+    }
+  }
+}
+```
+Botium setup is ready, you can begin to write your [BotiumScript](https://github.com/codeforequity-at/botium-core/wiki/Botium-Scripting) files.
+
+## Supported Capabilities
+
+Set the capability __CONTAINERMODE__ to __fbmessengerbots__ to activate this connector.
+
+### FB_PAGEID
+Facebook page id
+
+### FB_USER
+Faceboook username (email)
+
+### FB_PASSWORD
+Facebook password
---
+++ src/connector.js
@@ -0,0 +1,117 @@
+const util = require('util')
+const fblogin = require('facebook-chat-api')
+const debug = require('debug')('botium-connector-fbmessengerbots')
+const debugApi = require('debug')('botium-connector-fbmessengerbots-api')
+
+const Capabilities = {
+  FB_PAGEID: 'FB_PAGEID',
+  FB_USER: 'FB_USER',
+  FB_PASSWORD: 'FB_PASSWORD'
+}
+
+const promisify = (fnc, ...args) => {
+  return new Promise((resolve, reject) => {
+    try {
+      fnc(...args, (err, result) => {
+        if (err) reject(err)
+        else resolve(result)
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+class BotiumConnectorFBMessengerBots {
+  constructor ({ queueBotSays, caps }) {
+    this.queueBotSays = queueBotSays
+    this.caps = caps
+    this.fbapi = null
+    this.fbapiStopListener = null
+  }
+
+  async Validate () {
+    if (!this.caps[Capabilities.FB_PAGEID]) throw new Error('FB_PAGEID capability required')
+    if (!this.caps[Capabilities.FB_USER]) throw new Error('FB_USER capability required')
+    if (!this.caps[Capabilities.FB_PASSWORD]) throw new Error('FB_PASSWORD capability required')
+  }
+
+  async Build () {
+    this.fbapi = null
+    try {
+      this.fbapi = await promisify(fblogin,
+        { 
+          email: this.caps[Capabilities.FB_USER], 
+          password: this.caps[Capabilities.FB_PASSWORD] 
+        },
+        { 
+          logLevel: debugApi.enabled ? 'info' : 'warn' 
+        }
+      )
+
+      //const pageData = await promisify(this.fbapi.getUserID.bind(this.fbapi), `tonichatbot`)
+      //console.log(pageData)
+
+      debug(`Login to Facebook page ${this.caps[Capabilities.FB_PAGEID]} with user ${this.caps[Capabilities.FB_USER]} succeeded.`)
+    } catch (err) {
+      debug(`Login to Facebook page ${this.caps[Capabilities.FB_PAGEID]} with user ${this.caps[Capabilities.FB_USER]} failed: ${err.message || util.inspect(err)}.`)
+      if (err.error === 'login-approval') {
+        throw new Error('Two-factor authentiction required, which is not supported.')
+      } else {
+        throw new Error(`Facebook login failed: ${err.message || util.inspect(err)}`)
+      }
+    }
+  }
+
+  async Start () {
+    if (this.fbapiStopListener) {
+      this.fbapiStopListener()
+      this.fbapiStopListener = null
+    }
+    this.fbapiStopListener = this.fbapi.listenMqtt((err, event) => {
+      if (err) {
+        debug(`Facebook API Error: ${err.message || err}`)
+      } else if (event.type === 'message') {
+        debug(`Facebook API received message: ${util.inspect(event)}`)
+        const botMsg = { sourceData: event }
+        if (event.body) {
+          botMsg.messageText = event.body
+        }
+        this.queueBotSays(botMsg)
+      } else {
+        debug(`Facebook API ignored event: ${util.inspect(event)}`)
+      }
+    })
+  }
+
+  async UserSays (msg) {
+    debug(`UserSays called: ${msg.messageText}`)
+    this.fbapi.sendMessage(msg.messageText, `${this.caps[Capabilities.FB_PAGEID]}`)
+  }
+
+  async Stop () {
+    debug('Stop called')
+    if (this.fbapiStopListener) {
+      this.fbapiStopListener()
+      this.fbapiStopListener = null
+    }
+  }
+
+  async Clean () {
+    if (this.fbapiStopListener) {
+      this.fbapiStopListener()
+      this.fbapiStopListener = null
+    }
+    if (this.fbapi) {
+      try {
+        await promisify(this.fbapi.logout.bind(this.fbapi))
+        debug('Logout from Facebook ready.')
+      } catch (err) {
+        debug(`Logout from Facebook failed: ${err.message || util.inspect(err)}`)
+      }
+      this.fbapi = null
+    }
+  }
+}
+
+module.exports = BotiumConnectorFBMessengerBots
